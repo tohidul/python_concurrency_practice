62b22248846f12affc26a338e5ca92de5c4243c2
Md Tohidul Islam <tohidul.islam@dsinnovators.com>
2022-07-13 18:35:38 +0600

    added reactive programming example

---
+++ reactive_programming_example.py
@@ -0,0 +1,21 @@
+import rx
+
+
+class TemperatureObserver():
+    def on_next(self, x):
+        print("Temperature is %s degrees centigrade" % x)
+
+        if(x>6):
+            print("Warning: Temperate Is Exceeding Recommended Limit")
+        if(x==9):
+            print("DataCenter is shutting down. Temperature is too high")
+
+    def on_error(self, e):
+        print("Error: %s" % e)
+
+    def on_completed(self):
+        print("All Temps Read")
+
+xs = rx.from_iterable(range(10))
+
+d = xs.subscribe(TemperatureObserver())
